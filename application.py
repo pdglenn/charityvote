fd697c637b204e7dcc46057accb5d6b730fd54c1
Paul Glenn <paul.glenn@gmail.com>
2016-11-07 15:04:40 -0800

    Added routes and blank templates

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from application import forms, models
-from flask import (Flask, request, render_template, session, 
-    redirect, url_for, escape, flash, abort)
+from flask import Flask, render_template
 
 application = Flask(__name__)
 application.debug = True
@@ -8,7 +7,38 @@ application.secret_key = ('you_wont_guess')
 
 @application.route('/')
 def index():
-    return 'Hello world'
+    return render_template('index.html')
+
+
+@application.route('manage'):
+def manage():
+    return render_template('manage.html')
+
+
+@application.route('browse'):
+def browse():
+    return render_template('browse.html')
+
+
+@application.route('create'):
+def create():
+    return render_template('create.html')
+
+
+@application.route('view/<contest_id>'):
+def view(contest_id):
+    return render_template('view.html')
+
+
+@application.route('results/<contest_id>')
+def results(contest_id):
+    return render_template('results.html')
+
+
+@application.route('confirmation/<contest_id>')
+def confirmation(contest_id):
+    return render_template('confirmation.html')
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
