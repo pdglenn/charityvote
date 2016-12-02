e7fe04110f1bd692cfdc466828cd3693c4559044
Paul Glenn <paul.glenn@gmail.com>
2016-12-01 16:15:13 -0800

    Changed to pymysql

--- application/models.py
+++ application/models.py
@@ -1,4 +1,4 @@
-import MySQLdb
+import pymysql
 import uuid
 host = "charityvote.cloalfxvxpbz.us-west-2.rds.amazonaws.com"
 port = 3306
@@ -6,7 +6,7 @@ password ="tree1234"
 username = "root"
 
 def create_competition (title,amount,expiry_date):
-    db = MySQLdb.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
+    db = pymysql.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
     cursor = db.cursor()
     id =  uuid.uuid4()
     cursor.execute("insert into competitions values (%s,%s,%s,%s) ",(id,title,amount,expiry_date))
@@ -17,7 +17,7 @@ def create_competition (title,amount,expiry_date):
 
 
 def create_option (description,image_url,comp_id):
-    db = MySQLdb.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
+    db = pymysql.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
     cursor = db.cursor()
     id =  uuid.uuid4()
     ursor.execute("insert into comp_option values (%s,%s,%s,%s) ",(id,description,image_url,comp_id))
