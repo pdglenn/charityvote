24332185dffef4f907922d53142b23262cba78f0
Paul Glenn <paul.glenn@gmail.com>
2016-12-01 17:09:29 -0800

    Restructured models and config file

--- application.py
+++ application.py
@@ -1,11 +1,15 @@
-from application import forms, models
+from application import forms
 from flask import Flask, render_template, request
 import os
-UPLOAD_FOLDER = 'images/'
+
+
 application = Flask(__name__)
 application.debug = True
 application.secret_key = ('you_wont_guess')
-application.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+application.config.from_pyfile('config.py', silent=True)
+
+
 @application.route('/')
 def index():
     return render_template('index.html')
@@ -35,13 +39,13 @@ def create():
             title = cform.title.data
             amount = cform.amount.data
             date = cform.date.data
-            comp_id = models.create_competition(title,amount,date)
+            comp_id = create_competition(title,amount,date)
             for f in cform.options:
                 file_name = f.image_url
                 print ("OK" + str(file_name.name))
                 files = request.files[file_name.name]
                 image_url = os.path.join(application.config['UPLOAD_FOLDER'],files.filename)
-                models.create_option(f.description.data,image_url,id)
+                create_option(f.description.data,image_url,id)
                 files.save(image_url)
 
                 print(files.filename)
@@ -75,6 +79,26 @@ def confirmation(contest_id):
 def gen_confirmation():
     form = form
     return render_template('confirmation.html')
+
+host = application.config['SQL_HOST']
+port = 3306
+password = application.config['SQL_PASSWORD']
+username = application.config['SQL_USERNAME']
+
+def create_competition (title,amount,expiry_date):
+    db = pymysql.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
+    cursor = db.cursor()
+    id =  uuid.uuid4()
+    cursor.execute("insert into competitions values (%s,%s,%s,%s) ",(id,title,amount,expiry_date))
+    db.commit()
+    return id
+
+def create_option (description,image_url,comp_id):
+    db = pymysql.connect(host=host,user = username,passwd=password,db="charityvote",port=port)
+    cursor = db.cursor()
+    id =  uuid.uuid4()
+    cursor.execute("insert into comp_option values (%s,%s,%s,%s) ",(id,description,image_url,comp_id))
+    db.commit()
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0')
--- application/__init__.py
+++ application/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
 
 application = Flask(__name__)
-application.config.from_object('config')
+# application.config.from_object('config')
+application.config.from_pyfile('config.py', silent=True)
