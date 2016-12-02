a674310363ae10ef28ed72295b072e5d6b067dd0
Pratik Nadagouda <pratik@airbears2-10-142-29-238.airbears2.1918.berkeley.edu>
2016-12-01 18:35:36 -0800

    fixing buttons layout

--- static/main.js
+++ static/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
   $('.materialboxed').materialbox();
+
+  $('#login-trigger').click(function(){
+    $(this).next('#login-content').slideToggle();
+    $(this).toggleClass('active');
+
+    if ($(this).hasClass('active')) $(this).find('span').html('&#x25B2;')
+      else $(this).find('span').html('&#x25BC;')
+    })
 });
