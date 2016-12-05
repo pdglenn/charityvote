2ed14139e1a25e49e1bfad68e70a669d2cab8367
Paul Glenn <paul.glenn@gmail.com>
2016-12-05 13:11:29 -0800

    debugging with print

--- application/__init__.py
+++ application/__init__.py
@@ -3,5 +3,5 @@ from flask import Flask
 application = Flask(__name__)
 print('CONFIGGING WOO OWO')
 application.config.from_pyfile('config.py')
-application.config.from_pyfile('config.py')
+print('APP CONFIG PAUL', application.config)
 # application.config.from_envvar('PROD_CONFIG', silent=True)
