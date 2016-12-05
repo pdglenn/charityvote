fafee08d71419459cb70c658eab1ce9497bb19cd
Paul Glenn <paul.glenn@gmail.com>
2016-12-05 13:08:34 -0800

    trying to push changes here

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 application = Flask(__name__)
+print('CONFIGGING WOO OWO')
+application.config.from_pyfile('config.py')
 application.config.from_pyfile('config.py')
 # application.config.from_envvar('PROD_CONFIG', silent=True)
