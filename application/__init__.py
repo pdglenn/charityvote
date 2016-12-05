14d26481d83950ae49c5960a832b6fbc79b28fb8
Paul Glenn <paul.glenn@gmail.com>
2016-12-05 13:01:03 -0800

    config changes

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
 
 application = Flask(__name__)
-application.config.from_pyfile('/.ebextensions/config.py')
+application.config.from_pyfile('config.py')
 # application.config.from_envvar('PROD_CONFIG', silent=True)
