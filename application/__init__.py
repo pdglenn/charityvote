d377cfd09cc981283f9abab25d72544505c7bb2a
Paul Glenn <paul.glenn@gmail.com>
2016-12-05 12:17:03 -0800

    Trying to get configuration to work still

--- application/__init__.py
+++ application/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 application = Flask(__name__)
 # application.config.from_object('config')
-application.config.from_pyfile('config.py', silent=True)
+application.config.from_pyfile('../.ebextensions/config.py', silent=True)
